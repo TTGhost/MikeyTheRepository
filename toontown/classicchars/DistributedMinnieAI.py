510de1429f64d33ff9d7d4191497207deb798737
TTGhost <TTGhost@users.noreply.github.com>
2015-11-12 21:35:24 -0500

    Custom OTP version

--- toontown/classicchars/DistributedMinnieAI.py
+++ toontown/classicchars/DistributedMinnieAI.py
@@ -1,14 +1,14 @@
 # File: D (Python 2.4)
 
-from otp.ai.AIBaseGlobal import *
+from controlServers.AIBaseGlobal import *
 import DistributedCCharBaseAI
 from direct.directnotify import DirectNotifyGlobal
 from direct.fsm import ClassicFSM, State
 from direct.fsm import State
 from direct.task import Task
 import random
-from toontown.toonbase import ToontownGlobals
-from toontown.toonbase import TTLocalizer
+from controlServers.globals import ToontownGlobals
+from controlServers.globals import TTLocalizer
 import CharStateDatasAI
 
 class DistributedMinnieAI(DistributedCCharBaseAI.DistributedCCharBaseAI):
